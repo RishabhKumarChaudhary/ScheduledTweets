a9c272771ddfbebbe97d423cdddb7df353321204
RishabhKumarChaudhary <iamrishabhchaudhary2808@gmail.com>
2021-05-14 13:41:14 +0530

    Initial Commit
    Signed-off-by: RishabhKumarChaudhary <iamrishabhchaudhary2808@gmail.com>

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get "about", to: "about#index" ,as: :about;
+  get "contact", to: "contact#index", as: :contact ;
+  root to: "main#index"; 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
